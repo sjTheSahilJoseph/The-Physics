cff68506f1ca9c82f44cb89e06aa7d5a1584d722
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-05-05 19:38:08 -0700

    Classical Physics: Introduction

--- Classic_Physics_1/1_Introduction.c
+++ Classic_Physics_1/1_Introduction.c
@@ -2,5 +2,29 @@
 
 int main(int argc, char const* argv[]) {
     printf("Introduction\n");
+
+    // Introduction to Classic Physics
+
+    // Everything in universe operates on centain principles.
+    // Principles can be known, understood, and described
+    // using mathematics.
+    // For example, if we throw a an object form a tall building,
+    // mathematics can tell us what will happen.
+    // Like, how fast the ball will drop, where it'll be hit.
+    // This means the motion of every object in the universe ins't
+    // random, but quantomfiable, calculable, and predictable.
+    // Wheather we're talking about a pebble or a galaxy.
+
+    // The universe is math.
+
+    // Classicle physics doesn't work for the object that are:
+    // very small, or moving very fast, like speed of light.
+
+    // But we now know a lot about these things as well.
+    // Which is called "Modern Physics".
+
+    // We can understand "Modern Physics" after we understand
+    // Classical Physics. Since classical physics came first.
+    
     return 0;
 }
